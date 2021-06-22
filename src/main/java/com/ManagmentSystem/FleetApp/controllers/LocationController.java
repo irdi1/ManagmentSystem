2531e576833061a610e2734e84877d72e0206922
Irdi Elezi <irdielezi@gmail.com>
2021-06-22 04:02:45 +0300

    Location Done

--- src/main/java/com/ManagmentSystem/FleetApp/controllers/LocationController.java
+++ src/main/java/com/ManagmentSystem/FleetApp/controllers/LocationController.java
@@ -1,13 +1,64 @@
 package com.ManagmentSystem.FleetApp.controllers;
 
+import com.ManagmentSystem.FleetApp.models.Country;
+import com.ManagmentSystem.FleetApp.models.Location;
+import com.ManagmentSystem.FleetApp.models.State;
+import com.ManagmentSystem.FleetApp.services.CountryService;
+import com.ManagmentSystem.FleetApp.services.LocationService;
+import com.ManagmentSystem.FleetApp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LocationController {
 
+    @Autowired private LocationService locationService;
+    @Autowired private StateService stateService;
+    @Autowired private CountryService countryService;
+
+    // Get all the States
     @GetMapping("/locations")
-    public String getLocation() {
+    public String getStates(Model model) {
+        List<State> stateList = stateService.getStates();
+        model.addAttribute("states",stateList );
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries", countryList);
+        List<Location> locationList = locationService.getLocations();
+        model.addAttribute("locations", locationList);
+
         return "location";
+    }
+
+    // Add State
+    @PostMapping(value = "/locations/addNew")
+    public String addNew(Location location) {
+
+        locationService.save(location);
+
+        return "redirect:/locations";
+    }
+
+    @RequestMapping("/locations/findById")
+    @ResponseBody
+    public Optional<Location> findById(Integer id) {
+
+        return locationService.findById(id);
+    }
+
+    @RequestMapping(value = "/locations/updates", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value = "/locations/deletes", method = { RequestMethod.DELETE, RequestMethod.GET })
+    public String update(Integer id) {
+        locationService.delete(id);
+        return "redirect:/locations";
     }
 }
--- src/main/java/com/ManagmentSystem/FleetApp/services/LocationService.java
+++ src/main/java/com/ManagmentSystem/FleetApp/services/LocationService.java
@@ -1,7 +1,36 @@
 package com.ManagmentSystem.FleetApp.services;
 
+import com.ManagmentSystem.FleetApp.models.Location;
+import com.ManagmentSystem.FleetApp.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    // Return list of states
+    public List<Location> getLocations() {
+        return locationRepository.findAll();
+    }
+
+    //Save new Location
+    public void  save(Location location) {
+        locationRepository.save(location);
+    }
+
+    //Find Location by Id
+    public Optional<Location> findById(Integer id) {
+        return locationRepository.findById(id);
+    }
+
+    //Delete by Location id
+    public void delete(Integer id) {
+        locationRepository.deleteById(id);
+    }
 }
